b172363c6d5e88f9e12f4c67d504c45d3c7d20a9
jayarbautista <j.bautista@klab.com>
2014-08-22 08:57:42 +0800

    Simple onClick listener for buttons

--- SimpleButtons/src/com/android/exercises/simplebuttons/SimpleButtonActivity.java
+++ SimpleButtons/src/com/android/exercises/simplebuttons/SimpleButtonActivity.java
@@ -1,34 +1,39 @@
 package com.android.exercises.simplebuttons;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SimpleButtonActivity extends ActionBarActivity {
+	TextView mText;
+	Button mOkButton;
+	Button mCancelButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple_button);
+		mText = (TextView) findViewById(R.id.tvOut);
+		mOkButton = (Button) findViewById(R.id.ok_button);
+		mCancelButton = (Button) findViewById(R.id.cancel_button);
+		mOkButton.setOnClickListener(mOnClick);
+		mCancelButton.setOnClickListener(mOnClick);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.simple_button, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	OnClickListener mOnClick = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case (R.id.ok_button):
+				mText.setText("Button OK was clicked.");
+				break;
+			case (R.id.cancel_button):
+				mText.setText("Button Cancel was clicked.");
+				break;
+			}
 		}
-		return super.onOptionsItemSelected(item);
-	}
+	};
 }
